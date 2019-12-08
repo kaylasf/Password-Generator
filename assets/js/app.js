7bd8509cd6b32dbdc1a4c139467f974b608e32d4
kaylasf <sanfilippokayla@gmail.com>
2019-12-08 10:11:21 -0800

    copy to clipboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,14 +16,13 @@ slider.oninput = function () {
 }
 
 // CHECKBOX ONCLICKS //
-function checkBoxClick(e){
-    if (e.hasAttribute("checked")){
-        console.log("True to False")
+function checkBoxClick(e) {
+    if (e.hasAttribute("checked")) {
         e.removeAttribute('checked')
         e.setAttribute('value', 'f')
         return
     }
-    if (e.value === 'f'){
+    if (e.value === 'f') {
         e.setAttribute('value', 't')
     }
     else if (e.value === 't') {
@@ -33,56 +32,56 @@ function checkBoxClick(e){
 }
 
 // Declare create password function //
-function createPassword(len, spec, num, low, up){
+function createPassword(len, spec, num, low, up) {
     var specialChars = "!#$%&'()*+,-./:;<=>?@][\^_`{|}~"
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var passParams = []
     var passArr = []
-    if (spec == 't'){
+    if (spec == 't') {
         passParams.push('spec')
     }
-    if (num == 't'){
+    if (num == 't') {
         passParams.push('num')
     }
-    if (low == 't'){
+    if (low == 't') {
         passParams.push('low')
     }
-    if (up == 't'){
+    if (up == 't') {
         passParams.push('up')
     }
-    for (var i = 0; i < len; i++){
+    for (var i = 0; i < len; i++) {
         // Randomly choose paramter from passParams
         // Push character into passArr
-        var param = passParams[Math.floor(Math.random()*passParams.length)]
-        if (param === 'spec'){
+        var param = passParams[Math.floor(Math.random() * passParams.length)]
+        if (param === 'spec') {
             var splen = specialChars.length
-            ranSp = Math.floor(0 + Math.random()*(splen + 1 - 0))
+            ranSp = Math.floor(0 + Math.random() * (splen + 1 - 0))
             var sp = specialChars.charAt(ranSp)
             passArr.push(sp)
         }
-        if (param === 'up'){
+        if (param === 'up') {
             var alen = alphabet.length
-            ranAl = Math.floor(0 + Math.random()*(alen + 1 - 0))
+            ranAl = Math.floor(0 + Math.random() * (alen + 1 - 0))
             var al = alphabet.charAt(ranAl).toUpperCase()
             passArr.push(al)
         }
-        if (param === 'low'){
+        if (param === 'low') {
             var lolen = alphabet.length
-            ranLo = Math.floor(0 + Math.random()*(lolen + 1 - 0))
+            ranLo = Math.floor(0 + Math.random() * (lolen + 1 - 0))
             var lo = alphabet.charAt(ranLo).toLowerCase()
             passArr.push(lo)
         }
-        if (param === 'num'){
-            ranNum = Math.floor(0 + Math.random()*(9 + 1 - 0))
+        if (param === 'num') {
+            ranNum = Math.floor(0 + Math.random() * (9 + 1 - 0))
             var nu = ranNum.toString()
             passArr.push(nu)
         }
 
     }
-    
-    newPassword = passArr.join()
+
+    newPassword = passArr.join('')
     document.getElementById('newPass').innerHTML = newPassword
-    
+
 }
 
 
@@ -105,16 +104,11 @@ document.getElementById("submit").addEventListener("click", function (event) {
     var wantNum = numChar.value
     var wantLow = lowChar.value
     var wantUp = uppChar.value
-    console.log(slideInput, wantSpec, wantNum,wantLow, wantUp)
+    console.log(slideInput, wantSpec, wantNum, wantLow, wantUp)
     createPassword(slideInput, wantSpec, wantNum, wantLow, wantUp)
 
     //  var checkedItems = document.getElementsByClassName('checked')
 
-
-
-
-
-    // console.log(slideInput, speChar, numChar, lowChar, uppChar)
 });
 
 // SUBMIT BUTTON FUNCTIONALITY //
@@ -149,6 +143,58 @@ document.getElementById("submit").addEventListener("click", function (event) {
 //   $('#checkbox-value').text($('#checkbox1').val());
 // });
 
+
+// var t
+
+// function copyChange() {
+//     lightUp()
+//     t = setTimeout("stop()", 3000);
+// }
+
+
 function lightUp() {
-    document.getElementById("pwOutput").style.color = "red"
+    
+        document.getElementById("pwOutput").style.color = "red"
+ 
+
 }
+    // function stop() {
+    //     clearTimeout(t)
+    // }
+
+
+
+
+
+    // function changeBack() {
+    //     document.getElementById("pwdOutput").style.color = "black"
+    // }
+
+
+
+    function copyClipboard() {
+
+        var elm = document.getElementById("divClipboard");
+        // for Internet Explorer
+
+        if (document.body.createTextRange) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(elm);
+            range.select();
+            document.execCommand("Copy");
+            lightUp()
+           
+        }
+        else if (window.getSelection) {
+            // other browsers
+
+            var selection = window.getSelection();
+            var range = document.createRange();
+            range.selectNodeContents(elm);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            document.execCommand("Copy");
+            lightUp()
+            
+        }
+    }
